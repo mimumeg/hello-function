71cf26ac0ecb94e8f2182ce6ec8df17e2fbc8bfe
mimumeg <meg.stella555@gmail.com>
2018-11-18 09:21:08 +0900

    関数定義の方法を学んだ

---
+++ app.py
@@ -0,0 +1,30 @@
+def hello():
+    print("Hello World")
+
+
+hello()
+
+
+def say_hello(name):
+    print(f"Hi, {name}")
+
+
+say_hello('Bob')
+say_hello('Tom')
+
+
+def double(number):
+    return 2 * number
+
+
+result_1 = double(3)
+print(result_1)
+
+
+# 1分課題
+def str_combine(str1, str2):
+    return str1 + str2
+
+
+result = str_combine("Kazuma", "Takahashi")  # TakahashiKazumaを返す
+print(result)
